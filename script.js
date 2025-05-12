f95d5877640bdae44bee44570d340fae1bec9e0c
cosmincostea21 <cosmincostea0810@gmail.com>
2025-05-12 21:23:05 +0200

    incrementar

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ fetch('persona.json')
         
         personas.forEach(element => {
             let contenedorHijo = document.createElement('div');
-            contenedor.innerHTML = `
+            contenedor.innerHTML += `
                 <p>Nombre de la persona ${element.nombre}<p> <br>
                 <p>Edad de la persona ${element.edad}<p> <br>`;
             contenedor.appendChild(contenedorHijo);
